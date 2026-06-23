3222957d35e4ce5cfe213ae758007cffcaf9ac14
90Leater <90.leather@gmail.com>
2026-06-23 17:38:38 +0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -16,3 +16,44 @@ function updateCart(){
     );
 
 }
+document
+.getElementById('cartWhatsapp')
+.addEventListener(
+'click',
+() => {
+
+    if(selectedProducts.length === 0){
+        alert('Pilih produk terlebih dahulu');
+        return;
+    }
+
+    let pesan =
+    'Halo GO.Leather%0A%0ASaya ingin tanya harga:%0A%0A';
+
+    selectedProducts.forEach(id => {
+
+        const product =
+        products.find(
+            p => p.id === id
+        );
+
+        if(product){
+
+            pesan +=
+            '• ' +
+            product.nama +
+            ' (' +
+            product.kode +
+            ')%0A';
+
+        }
+
+    });
+
+    window.open(
+        'https://wa.me/6288973623416?text=' +
+        pesan,
+        '_blank'
+    );
+
+});
