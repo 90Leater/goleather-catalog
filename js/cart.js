ba9033b0f147eef2db5954027ce15d92514d990d
90Leater <90.leather@gmail.com>
2026-06-23 14:56:03 +0700

    Initialize selectedProducts from localStorage

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,18 @@
-let selectedProducts = [];
+let selectedProducts =
+    JSON.parse(
+    localStorage.getItem('selectedProducts')
+    ) || [];
+function updateCart(){
 
-function updateCart(){}
+    document.getElementById(
+    'cartCount'
+    ).textContent =
+
+    `🛒 ${selectedProducts.length} Produk Dipilih`;
+
+    localStorage.setItem(
+        'selectedProducts',
+        JSON.stringify(selectedProducts)
+    );
+
+}
