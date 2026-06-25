1bc7849d0bd79a5ed3978db16d46a2c0e110bff9
90Leater <90.leather@gmail.com>
2026-06-25 13:28:25 +0700

    Import modules and log admin load message

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,3 +1,13 @@
+import '../../firebase.js';
+
+import './import.js';
+import './products.js';
+import './categories.js';
+import './upload.js';
+import './auth.js';
+
+console.log('GO.Leather Admin Loaded');
+
 document
 .getElementById(
 'addProduct'
